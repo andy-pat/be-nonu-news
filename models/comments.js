1f3814c106edc696ca3d2314b42afc1c1bb442e3
Andy Patterson <andrewjp1992@gmail.com>
2020-12-10 16:42:40 +0000

    refactoring vote functionality

--- models/comments.js
+++ models/comments.js
@@ -1,19 +1,15 @@
-const connection = require('../db/connection')
+const connection = require("../db/connection");
 
 exports.patchVotes = (id, vote) => {
-    return connection('comments')
-        .returning('*')
-        .where('comment_id', id)
-        .increment('votes', vote)
-        .then((updatedComment) => {
-            return updatedComment
-        })
-
-}
+  return connection("comments")
+    .returning("*")
+    .where("comment_id", id)
+    .increment("votes", vote)
+    .then((updated) => {
+      return updated;
+    });
+};
 
 exports.deleteComment = (id) => {
-    return connection('comments')
-        .where('comment_id', id)
-        .del()
-
-}
+  return connection("comments").where("comment_id", id).del();
+};
