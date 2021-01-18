940d3a05d795d6cf66a9fa3c73f8ba61c49edcfc
Andy Patterson <andrewjp1992@gmail.com>
2021-01-18 15:09:19 +0000

    reseeding data

--- knexfile.js
+++ knexfile.js
@@ -1,31 +1,30 @@
 const { DB_URL } = process.env;
-const ENV = process.env.NODE_ENV || 'development';
-
+const ENV = process.env.NODE_ENV || "development";
 
 const baseConfig = {
-  client: 'pg',
+  client: "pg",
   migrations: {
-    directory: './db/migrations'
+    directory: "./db/migrations",
   },
   seeds: {
-    directory: './db/seeds'
-  }
+    directory: "./db/seeds",
+  },
 };
 
 const customConfig = {
   development: {
     connection: {
-      database: 'nc_news',
-      username: 'andy',
-      password: 'fuckpsql'
-    }
+      database: "nc_news",
+      username: "andy",
+      password: "fuckpsql",
+    },
   },
   test: {
     connection: {
-      database: 'nc_news_test',
-      username: 'andy',
-      password: 'fuckpsql'
-    }
+      database: "nc_news_test",
+      username: "andy",
+      password: "fuckpsql",
+    },
   },
   production: {
     connection: {
